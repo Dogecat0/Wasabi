21d754e650e3e92d872414da63760f8b86ef4a0c
Dogecat0 <zcmmwang@gmail.com>
2022-09-17 18:27:00 +0100

    Created recipe_index page template

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.recipes, name='recipes'),
+    path('', views.recipe_index, name='recipe_index'),
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from recipes.models import Recipes
 
 # Create your views here.
-def recipes(request):
-    return render(request, 'recipes.html', {})
+def recipe_index(request):
+    recipes = Recipes.objects.all()
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'recipe_index.html', context)
+
+def recipe_detail(request, pk):
+    recipe = Recipes.objects.get(pk=pk)
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'recipe_detail.html', context)
