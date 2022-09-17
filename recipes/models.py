04dbe18aae669a597c7c4b5cae3f3a48899c4a9e
Dogecat0 <zcmmwang@gmail.com>
2022-09-17 15:11:16 +0100

    Changed cooking_time field and updated initial.py

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Recipes(models.Model):
     title = models.CharField(max_length=50)
-    cooking_time = models.DecimalField(max_digits=3, decimal_places=1) # Cooking time normally is represented in hours(e.g 1.5h, 12h) or in minutes(e.g 50min)
+    cooking_time = models.CharField(max_length=10) # Cooking time normally is represented in hours(e.g 1.5h, 12h) or in minutes(e.g 50min)
     ingredients = models.TextField(default='ingredients displayed of this recipe when it is available')
     difficulty = models.PositiveSmallIntegerField(default=1)
     instructions = models.TextField(default='instructions displayed of this recipe when it is available') # How to cook in each step
