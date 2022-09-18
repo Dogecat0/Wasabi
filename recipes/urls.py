905b48e948fa10b6e08db8da6b78a435d6f151bf
Dogecat0 <zcmmwang@gmail.com>
2022-09-19 00:55:44 +0100

    Update formatting

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from recipes import views
 
 urlpatterns = [
-    path('', views.recipe_index, name='recipe_index'),
-    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path("", views.recipe_index, name="recipe_index"),
+    path("<int:pk>/", views.recipe_detail, name="recipe_detail"),
 ]
--- scraping/models.py
+++ scraping/models.py
@@ -1,5 +1,3 @@
-from multiprocessing import context
-
 from django.db import models
 
 # Create your models here.
