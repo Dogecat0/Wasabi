44f608e9cba28ad2182205e64f72391de1fb1962
Dogecat0 <zcmmwang@gmail.com>
2022-09-19 13:24:10 +0100

    Updates to recipes views

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path("", views.recipe_index, name="recipe_index"),
-    path("<int:pk>/", views.recipe_detail, name="recipe_detail"),
+    path("", views.index, name="index"),
+    path("<int:pk>/", views.detail, name="detail"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,13 +4,13 @@ from recipes.models import Recipe
 
 
 # Create your views here.
-def recipe_index(request):
+def index(request):
     recipes = Recipe.objects.all()
     context = {"recipes": recipes}
-    return render(request, "recipe_index.html", context)
+    return render(request, "index.html", context)
 
 
-def recipe_detail(request, pk):
+def detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     context = {"recipe": recipe}
-    return render(request, "recipe_detail.html", context)
+    return render(request, "detail.html", context)
