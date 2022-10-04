642ec69577d186a2ffd00e45e0a85600575ce85f
Dogecat0 <zcmmwang@gmail.com>
2022-10-04 16:59:07 +0100

    Update to settings secret keys

--- wasabi_server/settings.py
+++ wasabi_server/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "wasabi_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "wasabi",
+        "USER": "postgres",
+        "PASSWORD": str(environ["DJANGO_DATABASE_PASSWORD"]),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
