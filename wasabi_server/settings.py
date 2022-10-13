3b3fb803042f74c86621fc27203479a9b234bdf1
Dogecat0 <zcmmwang@gmail.com>
2022-10-13 23:15:44 +0100

    Load images to home page

--- wasabi_server/settings.py
+++ wasabi_server/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/images/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "recipes/static")]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
